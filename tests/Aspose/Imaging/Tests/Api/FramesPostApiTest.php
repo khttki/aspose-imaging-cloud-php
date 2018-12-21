eb5b0916bd8c7578b78d7b3d5bd6ac3f935ee7cc
Maksym Shnurenok <maksym.shnurenok@aspose.com>
2018-12-21 20:06:23 +0200

    IMAGINGCLOUD-212 - Finished porting Imaging tests.

---
+++ tests/Aspose/Imaging/Tests/Api/FramesPostApiTest.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * --------------------------------------------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="FramesPostApiTest.php">
+ *   Copyright (c) 2019 Aspose Pty Ltd. All rights reserved.
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------------------------------------------
+ */
+
+namespace Aspose\Imaging\Tests\Api;
+
+use \Aspose\Imaging\Tests\Base\ApiTester;
+use \Aspose\Imaging;
+use \Aspose\Imaging\Model;
+use \Aspose\Imaging\Model\Requests;
+use \PHPUnit\Framework\TestCase;
+use \GuzzleHttp\Stream;
+
+/**
+ * Class for testing FramesApi
+ * 
+ * @group Imaging
+ * @group v2.0
+ * @group Tiff
+ */
+class FramesPostApiTest extends ApiTester
+{
+    /**
+     * Test PostImageFrame
+     * 
+     * @test
+     * @dataProvider storageOptionsProvider
+     *
+     * @param bool $saveResultToStorage If result should be saved to storage.
+     * @return void
+    */
+    public function postImageSingleFrameTest($saveResultToStorage)
+    {
+        $name = "test.tiff";
+        $frameId = 2;
+        $newWidth = 300;
+        $newHeight = 450;
+        $x = 10;
+        $y = 10;
+        $rectWidth = 200;
+        $rectHeight = 300;
+        $rotateFlipMethod = "Rotate90FlipX";
+        $saveOtherFrames = false;
+        $outName = $name . "_singleFrame.tiff";
+        $folder = self::$tempFolder;
+        $storage = self::$testStorage;
+
+        $this->postRequestTestInternal(
+            "postImageSingleFrameTest", 
+            $saveResultToStorage,
+            "Input image: " . $name . "; Frame ID: " . $frameId . "; New width: " . $newWidth . "; New height: " . $newHeight . "; X: " . $x. "; Y: " . $y . 
+                "; Rect width: " . $rectWidth . "; Rect height: " . $rectHeight . "; Rotate/flip method: " . $rotateFlipMethod . 
+                "; Save other frames: " . var_export($saveOtherFrames, true),
+            $name,
+            $outName,
+            function($inputStream, $outPath) use ($frameId, $newWidth, $newHeight, $x, $y, $rectWidth, $rectHeight , $rotateFlipMethod, $saveOtherFrames, $storage)
+            {
+                $request = new Requests\PostImageFrameRequest($inputStream, $frameId, $newWidth, $newHeight, $x, $y, $rectWidth, $rectHeight, 
+                    $rotateFlipMethod, $saveOtherFrames, $outPath, $storage);
+                return self::$asyncMode === true ? self::$imagingApi->postImageFrameAsync($request)->wait() : self::$imagingApi->postImageFrame($request);
+            },
+            function($originalProperties, $resultProperties, $resultStream) use ($frameId, $newWidth, $newHeight, $x, $y, $rectWidth, $rectHeight, 
+                $rotateFlipMethod, $saveOtherFrames, $saveResultToStorage, $outName, $folder, $storage)
+            {
+                $this->assertNotNull($resultProperties->getTiffProperties());
+                $this->assertNotNull($resultProperties->getTiffProperties()->getFrames());
+                $this->assertEquals(1, count($resultProperties->getTiffProperties()->getFrames()));
+
+                // please note that rotation was performed, so switching of width and height comparison is valid
+                $this->assertEquals($rectHeight, $resultProperties->getTiffProperties()->getFrames()[0]->getWidth());
+                $this->assertEquals($rectWidth, $resultProperties->getTiffProperties()->getFrames()[0]->getHeight());
+                $this->assertEquals($resultProperties->getTiffProperties()->getFrames()[0]->getFrameOptions()->getImageWidth(), $rectHeight);
+                $this->assertEquals($rectWidth, $resultProperties->getTiffProperties()->getFrames()[0]->getFrameOptions()->getImageLength());
+                $this->assertEquals($rectHeight, $resultProperties->getWidth());
+                $this->assertEquals($rectWidth, $resultProperties->getHeight());
+
+                if (!$saveResultToStorage) return;
+
+                $framePropertiesRequest = new Requests\GetImageFramePropertiesRequest($outName, 0, $folder, $storage);
+                $framePropertiesResponse = self::$asyncMode === true ? 
+                    self::$imagingApi->getImageFramePropertiesAsync($framePropertiesRequest)->wait() :
+                    self::$imagingApi->getImageFrameProperties($framePropertiesRequest);
+
+                $this->assertNotNull($framePropertiesResponse);
+                $this->assertNotNull($framePropertiesResponse->getTiffProperties());
+                $this->assertNotNull($framePropertiesResponse->getTiffProperties()->getFrames());
+                $this->assertEquals($rectHeight, $framePropertiesResponse->getWidth());
+                $this->assertEquals($rectWidth, $framePropertiesResponse->getHeight());
+                $this->assertEquals($framePropertiesResponse->getTiffProperties()->getFrames()[0]->getWidth(), $framePropertiesResponse->getWidth());
+                $this->assertEquals($framePropertiesResponse->getTiffProperties()->getFrames()[0]->getHeight(), $framePropertiesResponse->getHeight());
+                $this->assertEquals($framePropertiesResponse->getTiffProperties()->getFrames()[0]->getFrameOptions()->getImageWidth(), $framePropertiesResponse->getWidth());
+                $this->assertEquals($framePropertiesResponse->getTiffProperties()->getFrames()[0]->getFrameOptions()->getImageLength(), $framePropertiesResponse->getHeight());
+            },
+            $folder,
+            $storage);
+    }
+
+    /**
+     * Test PostImageFrame
+     * 
+     * @test
+     * @dataProvider storageOptionsProvider
+     *
+     * @param bool $saveResultToStorage If result should be saved to storage.
+     * @return void
+    */
+    public function postImageAllFramesTest($saveResultToStorage)
+    {
+        $name = "test.tiff";
+        $frameId = 2;
+        $newWidth = 300;
+        $newHeight = 450;
+        $x = 10;
+        $y = 10;
+        $rectWidth = 200;
+        $rectHeight = 300;
+        $rotateFlipMethod = "Rotate90FlipX";
+        $saveOtherFrames = true;
+        $outName = $name . "_allFrames.tiff";
+        $folder = self::$tempFolder;
+        $storage = self::$testStorage;
+
+        $this->postRequestTestInternal(
+            "postImageAllFramesTest", 
+            $saveResultToStorage,
+            "Input image: " . $name . "; Frame ID: " . $frameId . "; New width: " . $newWidth . "; New height: " . $newHeight . "; X: " . $x. "; Y: " . $y . 
+                "; Rect width: " . $rectWidth . "; Rect height: " . $rectHeight . "; Rotate/flip method: " . $rotateFlipMethod . 
+                "; Save other frames: " . var_export($saveOtherFrames, true),
+            $name,
+            $outName,
+            function($inputStream, $outPath) use ($frameId, $newWidth, $newHeight, $x, $y, $rectWidth, $rectHeight , $rotateFlipMethod, $saveOtherFrames, $storage)
+            {
+                $request = new Requests\PostImageFrameRequest($inputStream, $frameId, $newWidth, $newHeight, $x, $y, $rectWidth, $rectHeight, 
+                    $rotateFlipMethod, $saveOtherFrames, $outPath, $storage);
+                return self::$asyncMode === true ? self::$imagingApi->postImageFrameAsync($request)->wait() : self::$imagingApi->postImageFrame($request);
+            },
+            function($originalProperties, $resultProperties, $resultStream) use ($frameId, $newWidth, $newHeight, $x, $y, $rectWidth, $rectHeight, 
+                $rotateFlipMethod, $saveOtherFrames, $saveResultToStorage, $outName, $folder, $storage)
+            {
+                $this->assertNotNull($originalProperties->getTiffProperties());
+                $this->assertNotNull($originalProperties->getTiffProperties()->getFrames());
+
+                $this->assertNotNull($resultProperties->getTiffProperties());
+                $this->assertNotNull($resultProperties->getTiffProperties()->getFrames());
+
+                $this->assertEquals(count($originalProperties->getTiffProperties()->getFrames()), count($resultProperties->getTiffProperties()->getFrames()));
+                $this->assertEquals($originalProperties->getWidth(), $resultProperties->getWidth());
+                $this->assertEquals($originalProperties->getHeight(), $resultProperties->getHeight());
+
+                // please note that rotation was performed, so switching of width and height comparison is valid
+                $this->assertEquals($rectHeight, $resultProperties->getTiffProperties()->getFrames()[$frameId]->getWidth());
+                $this->assertEquals($rectWidth, $resultProperties->getTiffProperties()->getFrames()[$frameId]->getHeight());
+                $this->assertEquals($rectHeight, $resultProperties->getTiffProperties()->getFrames()[$frameId]->getFrameOptions()->getImageWidth());
+                $this->assertEquals($rectWidth, $resultProperties->getTiffProperties()->getFrames()[$frameId]->getFrameOptions()->getImageLength());
+            },
+            $folder,
+            $storage);
+    }
+}
---
+++ tests/Aspose/Imaging/Tests/Api/RotateFlipApiTest.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * --------------------------------------------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="RotateFlipApiTest.php">
+ *   Copyright (c) 2019 Aspose Pty Ltd. All rights reserved.
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------------------------------------------
+ */
+
+namespace Aspose\Imaging\Tests\Api;
+
+use \Aspose\Imaging\Tests\Base\ApiTester;
+use \Aspose\Imaging;
+use \Aspose\Imaging\Model;
+use \Aspose\Imaging\Model\Requests;
+use \PHPUnit\Framework\TestCase;
+use \GuzzleHttp\Stream;
+
+/**
+ * Class for testing RotateFlipApi
+ * 
+ * @group Imaging
+ * @group v1.0
+ * @group v2.0
+ * @group RotateFlip
+ */
+class RotateFlipApiTest extends ApiTester
+{
+    /**
+     * Export options provider
+     *
+     * @return array
+     */
+    public function exportOptionsProvider()
+    {
+        return [
+            [".bmp", true],  [".bmp", false], 
+            [".dicom", true], [".dicom", false], 
+            /* TODO: enable after IMAGINGCLOUD-51 is resolved
+            [".gif", true], [".gif", false], 
+            */
+            [".j2k", true], [".j2k", false],
+            [".png", true], [".png", false],
+            [".psd", true], [".psd", false],
+            [".jpg", true], [".jpg", false],
+            [".tiff", true], [".tiff", false],
+            [".webp", true], [".webp", false]
+        ];
+    }
+
+    /**
+     * Test GetImageRotateFlip
+     * 
+     * @test
+     * @dataProvider exportOptionsProvider
+     *
+     * @param string $formatExtension Format extension to search for input images in the test folder.
+     * @param bool $saveResultToStorage If result should be saved to storage.
+     * @param array $additionalExportFormats Additional formats to export to.
+     * @return void
+    */
+    public function getImageRotateFlipTest($formatExtension, $saveResultToStorage, $additionalExportFormats = [])
+    {
+        $name = null;
+        $method = "Rotate90FlipX";
+        $folder = self::$tempFolder;
+        $storage = self::$testStorage;
+        $outName = null;
+
+        $formatsToExport = ApiTester::BasicExportFormats;
+        foreach($additionalExportFormats as $additionalExportFormat)
+        {
+            if (!in_array($additionalExportFormat, $formatsToExport))
+            {
+                $formatsToExport[] = $additionalExportFormat;
+            }
+        }
+
+        foreach (self::$inputTestFiles as $inputFile)
+        {
+            $inputFileName = $inputFile->getName();
+            if (substr($inputFileName, -strlen($formatExtension), strlen($formatExtension)) === $formatExtension)
+            {
+                $name = $inputFileName;
+            }
+            else
+            {
+                continue;
+            }
+
+            foreach ($formatsToExport as $format)
+            {
+                $outName = $name . "_rotateFlip." . $format;
+
+                $this->getRequestTestInternal(
+                    "getImageRotateFlipTest", 
+                    $saveResultToStorage,
+                    "Input image: " . $name . "; Output format: " . $format . "; Method: " . $method,
+                    $name,
+                    $outName,
+                    function($fileName, $outPath) use ($format, $method, $folder, $storage)
+                    {
+                        $request = new Requests\GetImageRotateFlipRequest($fileName, $format, $method, $outPath, $folder, $storage);
+                        return self::$asyncMode === true ? self::$imagingApi->getImageRotateFlipAsync($request)->wait() : self::$imagingApi->getImageRotateFlip($request);
+                    },
+                    function($originalProperties, $resultProperties, $resultStream)
+                    {
+                        try
+                        {
+                            $this->assertEquals($originalProperties->getHeight(), $resultProperties->getWidth());
+                        }
+                        catch (\Exception $ex)
+                        {
+                            $this->assertEquals($originalProperties->getHeight(), $resultProperties->getWidth() + 1);
+                        }
+
+                        try
+                        {
+                            $this->assertEquals($originalProperties->getWidth(), $resultProperties->getHeight());
+                        }
+                        catch (\Exception $ex)
+                        {
+                            $this->assertEquals($originalProperties->getWidth(), $resultProperties->getHeight() + 1);
+                        }
+                    },
+                    $folder,
+                    $storage);
+            }
+        }
+    }
+
+    /**
+     * Test PostImageRotateFlip
+     * 
+     * @test
+     * @dataProvider exportOptionsProvider
+     *
+     * @param string $formatExtension Format extension to search for input images in the test folder.
+     * @param bool $saveResultToStorage If result should be saved to storage.
+     * @param array $additionalExportFormats Additional formats to export to.
+     * @return void
+    */
+    public function postImageRotateFlipTest($formatExtension, $saveResultToStorage, $additionalExportFormats = [])
+    {
+        $name = null;
+        $method = "Rotate90FlipX";
+        $folder = self::$tempFolder;
+        $storage = self::$testStorage;
+        $outName = null;
+
+        $formatsToExport = ApiTester::BasicExportFormats;
+        foreach($additionalExportFormats as $additionalExportFormat)
+        {
+            if (!in_array($additionalExportFormat, $formatsToExport))
+            {
+                $formatsToExport[] = $additionalExportFormat;
+            }
+        }
+
+        foreach (self::$inputTestFiles as $inputFile)
+        {
+            $inputFileName = $inputFile->getName();
+            if (substr($inputFileName, -strlen($formatExtension), strlen($formatExtension)) === $formatExtension)
+            {
+                $name = $inputFileName;
+            }
+            else
+            {
+                continue;
+            }
+
+            foreach ($formatsToExport as $format)
+            {
+                $outName = $name . "_rotateFlip." . $format;
+
+                $this->postRequestTestInternal(
+                    "postImageRotateFlipTest", 
+                    $saveResultToStorage,
+                    "Input image: " . $name . "; Output format: " . $format . "; Method: " . $method,
+                    $name,
+                    $outName,
+                    function($inputStream, $outPath) use ($format, $method, $storage)
+                    {
+                        $request = new Requests\PostImageRotateFlipRequest($inputStream, $format,$method, $outPath, $storage);
+                        return self::$asyncMode === true ? self::$imagingApi->postImageRotateFlipAsync($request)->wait() : self::$imagingApi->postImageRotateFlip($request);
+                    },
+                    function($originalProperties, $resultProperties, $resultStream)
+                    {
+                        try
+                        {
+                            $this->assertEquals($originalProperties->getHeight(), $resultProperties->getWidth());
+                        }
+                        catch (\Exception $ex)
+                        {
+                            $this->assertEquals($originalProperties->getHeight(), $resultProperties->getWidth() + 1);
+                        }
+
+                        try
+                        {
+                            $this->assertEquals($originalProperties->getWidth(), $resultProperties->getHeight());
+                        }
+                        catch (\Exception $ex)
+                        {
+                            $this->assertEquals($originalProperties->getWidth(), $resultProperties->getHeight() + 1);
+                        }
+                    },
+                    $folder,
+                    $storage);
+            }
+        }
+    }
+}
